233768b62fe1496397dfbf68e8edf0b0934c8205
Krish <krishshah8590@gmail.com>
2024-05-27 19:34:51 -0700

    Links

--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className="bg-dark text-white text-center py-3" id="contact">
       <Container>
         <p>
-          <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-white">LinkedIn</a>&nbsp;-&nbsp;
+          <a href="https://www.linkedin.com/in/krishshah273/" target="_blank" rel="noopener noreferrer" className="text-white">LinkedIn</a>&nbsp;-&nbsp;
           
-          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-white">GitHub</a>
+          <a href="https://github.com/KrishShah1" target="_blank" rel="noopener noreferrer" className="text-white">GitHub</a>
         </p>
       </Container>
     </footer>
